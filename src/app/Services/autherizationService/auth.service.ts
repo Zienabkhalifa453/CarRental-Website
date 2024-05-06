6e250a9157de1bb26fa5e8ba99bda14683f37dae
Reem <reem.osama3052001@gmail.com>
2024-05-07 00:13:04 +0300

    creat environment,auth service

---
+++ src/app/Services/autherizationService/auth.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private _httpclient:HttpClient) { }
+
+
+  setLogin(loginData:object):Observable<any>{
+
+return this._httpclient.post(`${environment.baseURL}//api/account/login`,loginData);
+
+  }
+}
