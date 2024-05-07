ab8b7b5d46b268d5e0a4cffbf88f881e680cf62a
maio111 <maiomohamed707@gmail.com>
2024-05-07 23:54:22 +0300

    Master CarServices

--- src/app/components/cars/cars.component.ts
+++ src/app/components/cars/cars.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CarApiService } from '../../Services/car-api.service';
 
 @Component({
   selector: 'app-cars',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cars.component.html',
   styleUrl: './cars.component.css'
 })
 export class CarsComponent {
+  cars:any[]=[];
+  currentPage: number = 1;
+  constructor(private carService:CarApiService)
+  {
 
+  }
+  ngOnInit() {
+    this.fetchCarsByPage(this.currentPage);
+  }
+
+  fetchCarsByPage(page: number) {
+    this.carService.getCarsByPage(page).subscribe((res) => {
+      this.cars = res.message;
+    });
+  }
+
+  onPageChange(page: number) {
+    this.currentPage = page;
+    this.fetchCarsByPage(page);
+  }
 }
