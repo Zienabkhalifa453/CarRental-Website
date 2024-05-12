98b77200c133f62c4eb255e42e080359713d7bf1
zienab alaa <zienabalaa442@gmail.com>
2024-05-12 15:11:04 +0300

    register Done

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -4,16 +4,25 @@ import { FormControl } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
+
+import { AuthService } from '../services/auth.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
 import { CommonModule } from '@angular/common';
 
 @Component({
+  
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink,ReactiveFormsModule,FormsModule,CommonModule],
+  imports: [RouterLink,ReactiveFormsModule,FormsModule,HttpClientModule,BrowserModule,CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
+
+
 export class RegisterComponent {
+
+  constructor( private _AuthService:AuthService){}
   registerForm:FormGroup=new FormGroup({
   Name : new FormControl(null,[Validators.required,Validators.minLength(3)]),
   Email: new FormControl(null,[Validators.required,Validators.email]),
@@ -22,12 +31,24 @@ export class RegisterComponent {
   password: new FormControl(null,[Validators.required,Validators.pattern(/[A-Z][a-z0-9]{6,}/)]),
   rePassword: new FormControl(null,[Validators.required,Validators.pattern(/[A-Z][a-z0-9]{6,}/)]),
   Address: new FormControl(null,[Validators.required,Validators.minLength(3)]),
+  Role: new FormControl("CUSTOMER")
   
 
 
 
   });
   handleForm():void{
-    console.log(this.registerForm);
+    if(this.registerForm.valid){
+    
+    this._AuthService.setRegister(this.registerForm.value).subscribe({
+      next:(response)=>{
+        console.log(response)
+      },
+      error:(err)=>{
+        console.log(err)
+
+      }
+    })
+    }
   }
 }
